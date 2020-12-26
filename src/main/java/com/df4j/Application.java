f7f6740aef099997ffaa9cad653a38b10f77a2a6
wangxc <wxc252@hotmail.com>
2020-12-26 16:48:25 +0800

    初始化提交

---
+++ src/main/java/com/df4j/Application.java
@@ -0,0 +1,19 @@
+package com.df4j;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+    private static Logger logger = LoggerFactory.getLogger(Application.class);
+
+    public static void main(String[] args) {
+        try {
+            SpringApplication.run(Application.class, args);
+        } catch (Throwable e) {
+            logger.error("启动异常", e);
+        }
+    }
+}
