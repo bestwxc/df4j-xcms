7e27c0e9c7c790f3cb10981d988d68d7a28bef60
ywz135 <wxc252@hotmail.com>
2021-01-30 17:05:01 +0800

    修改部署的脚本

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@
 ## 目标目录
 target_dir=/srv/xcms
 ## 定义
-artifact_id=1.0.0-SNAPSHOT
+artifact_id=df4j-xcms
 varsion=1.0.0-SNAPSHOT
 
 ## jar 名称
@@ -12,7 +12,7 @@ jar_name=$artifact_id-$varsion.jar
 cur_dir=$(cd `dirname $0`;pwd)
 
 ## 创建目标目录
-mkdir -p target_dir
+mkdir -p $target_dir
 
 ## 拷贝包
 \cp -f "$cur_dir"/target/$jar_name $target_dir
@@ -25,4 +25,4 @@ echo "java -jar $target_dir/$jar_name &"
 ## 启动
 nohup java -jar $target_dir/$jar_name &
 
-
+tail -fn 200 nohup.out
